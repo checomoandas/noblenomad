16ed2491fb09b3a418e321db19b09b59532f03b0
checomoandas <155914335+checomoandas@users.noreply.github.com>
2024-02-05 01:46:58 -0300

    Update Vs27x.js

--- Vs27x.js
+++ Vs27x.js
@@ -325,7 +325,7 @@ function updateSidebarWithMarkerData(processedData) {
 
 
 // Assuming the CSV file is hosted at a reachable URL
-let csvUrl = 'YOUR_CSV_FILE_URL_HERE';
+let csvUrl = 'https://raw.githubusercontent.com/checomoandas/noblenomad/main/BsAsPins.csv';
 
 // Function to fetch and parse CSV data
 function loadCsvData(url) {
